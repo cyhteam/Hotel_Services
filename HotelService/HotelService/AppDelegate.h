ff4138aebda50f8236b07189373d3cde29d4f9fa
Nguyen Manh Cuong <cuongcnb@gmail.com>
2014-11-24 19:21:53 +0700

    Initial Commit

---
+++ HotelService/HotelService/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelService
+//
+//  Created by Nguyen Manh Cuong on 11/24/14.
+//  Copyright (c) 2014 vnpt-technology.vn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
